5360eb9095fdb26b73e6a3244b102dbc4ba96c04
bescoss <pablo.bescos@sarria.salesians.cat>
2023-10-27 16:20:44 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World Pablo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Hello, World!'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Pablo Bescós Curs 23-24 Salesians de Sarrià',
-            ),
-          ],
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello, world! Pablo Bescós"),
+          backgroundColor: Colors.blue,
+        ),
+        body: const Center(
+          child: Column(
+            children: <Widget>[
+              Text('Pablo Bescós Curs 23-24 Salesians de Sarrià',
+                  style: TextStyle(
+                    fontSize: 30,
+                  )),
+            ],
+          ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
